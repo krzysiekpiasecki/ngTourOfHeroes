52409ad76c0e85393fd0f576d25510880bb88197
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2017-01-22 08:38:58 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+import './rxjs-extensions';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroSearchComponent
+  ],
+  providers:    [ HeroService ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
+
+
+
+
